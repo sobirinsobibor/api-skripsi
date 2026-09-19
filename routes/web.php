2ffff07f68380a61670d5b817a7f5b0bcc35f8f1
sobirin sobibor <wsobrin2@gmail.com>
2026-09-19 22:58:06 +0700

    test action 1

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\UndergraduateThesisController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'welcome';
+    return 'welcome to api';
 });
 
 
