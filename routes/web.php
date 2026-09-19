68d65a7d3eade968fb374591e26ccfe941bddcf5
sobirin sobibor <wsobrin2@gmail.com>
2026-09-19 22:53:47 +0700

    test action

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\Api\UndergraduateThesisController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return 'welcome';
+});
+
+
 Route::prefix('undergraduate-theses')->group(function () {
     Route::get('/', [UndergraduateThesisController::class, 'index']);    // Get All / Paginated
     Route::get('/{id}', [UndergraduateThesisController::class, 'show']); // Detail by ID
